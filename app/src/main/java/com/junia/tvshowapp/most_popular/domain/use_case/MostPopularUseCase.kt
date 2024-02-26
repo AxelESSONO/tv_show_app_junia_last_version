87d984b9e23ef29874b078ba05daa346b18aec91
axelessono <naxelcdric@gmail.com>
2024-02-26 09:31:52 +0100

    MostPopular Use Case

--- app/src/main/java/com/junia/tvshowapp/most_popular/domain/use_case/MostPopularUseCase.kt
+++ app/src/main/java/com/junia/tvshowapp/most_popular/domain/use_case/MostPopularUseCase.kt
@@ -1,12 +1,33 @@
 package com.junia.tvshowapp.most_popular.domain.use_case
 
+import com.junia.tvshowapp.most_popular.data.dto.toTvShow
+import com.junia.tvshowapp.most_popular.domain.model.TvShow
 import com.junia.tvshowapp.most_popular.domain.repository.MostPopularRepository
+import com.junia.tvshowapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 class MostPopularUseCase @Inject constructor(
     private val mostPopularRepository: MostPopularRepository
 ) {
 
-    //operator fun invoke(page : Int)
+    operator fun invoke(page : Int) : Flow<Resource<List<TvShow>>> = flow {
+
+        try {
+            emit(Resource.Loading<List<TvShow>>(isLoading = true))
+            val tvShows : List<TvShow> = mostPopularRepository.getMostPopularTvShows(page)
+                .tvShows.map {
+                    it.toTvShow()
+                }
+            emit(Resource.Success<List<TvShow>>(tvShows))
+        }catch (e : IOException){
+            emit(Resource.Error<List<TvShow>>(message = "${e.localizedMessage}"))
+        }catch (e : HttpException){
+            emit(Resource.Error<List<TvShow>>(message = "Internet Error"))
+        }
+    }
 
 }

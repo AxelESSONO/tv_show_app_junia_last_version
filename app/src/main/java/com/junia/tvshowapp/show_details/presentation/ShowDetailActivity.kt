7c25811f3226a27da56303e7d869d9edb8e6e477
axelessono <naxelcdric@gmail.com>
2024-02-28 20:38:23 +0100

    Show TvShow Detail

---
+++ app/src/main/java/com/junia/tvshowapp/show_details/presentation/ShowDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.junia.tvshowapp.show_details.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.junia.tvshowapp.R
+
+class ShowDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_detail)
+    }
+}
